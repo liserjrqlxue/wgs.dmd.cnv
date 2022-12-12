88e8a07746d9d50f5fceee4aba4e0625197d8f40
Wang Yaoshen <wangyaoshen@bgi.com>
2022-12-12 18:00:47 +0800

    :bug:in case a$fixRatio==0

--- bin/cbs.R
+++ bin/cbs.R
@@ -7,7 +7,7 @@ a<-read.table(input,stringsAsFactors=T,sep="\t",fill=T,header=T)
 cna<-segment(
     smooth.CNA(
         CNA(
-            log2(a$fixRatio),
+            log2(a$fixRatio+0.00001),
             a$chr,
             a$start,
             data.type = "logratio",
